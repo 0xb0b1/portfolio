ac90503004a2f17ef32049f366d23ef0cbded021
0xb0b1 <vcente82@gmail.com>
2021-03-06 12:09:45 -0300

    ...

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hello Paulo</h1>
+        </div>
+    );
+}
